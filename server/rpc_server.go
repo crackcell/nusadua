21442ce3483267c92034c38a055f3c02b7a3316a
Menglong TAN <tanmenglong@gmail.com>
2017-05-03 14:56:30 +0800

    add rpc

---
+++ server/rpc_server.go
@@ -0,0 +1,31 @@
+/***************************************************************
+ *
+ * Copyright (c) 2017, Menglong TAN <tanmenglong@gmail.com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the Apache licence
+ *
+ **************************************************************/
+
+/**
+ *
+ *
+ * @file rpc_server.go
+ * @author Menglong TAN <tanmenglong@gmail.com>
+ * @date Wed May  3 14:54:44 2017
+ *
+ **/
+
+package server
+
+//===================================================================
+// Public APIs
+//===================================================================
+
+//===================================================================
+// Private
+//===================================================================
+
+func main() {
+
+}
--- thrift/nusadua.thrift
+++ thrift/nusadua.thrift
@@ -1,11 +1,12 @@
-namespace go server
+namespace go rpc
+namespace java com.crackcell.nusadua.rpc
 
-exception ParameterServerException {
+exception ParameterServiceException {
     1:i32 status,
     2:string message
 }
 
-service ParameterServer {
-    void push(1:list<i64> keys, 2:list<double> values) throws (1:ParameterServerException exp),
-    list<double> pull(1:list<i64> keys) throws (1:ParameterServerException exp)
+service ParameterService {
+    void push(1:list<i64> keys, 2:list<double> values) throws (1:ParameterServiceException ex),
+    list<double> pull(1:list<i64> keys) throws (1:ParameterServiceException ex)
 }
