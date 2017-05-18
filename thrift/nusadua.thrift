cfcaf0941e1e8063ee2949e321a5b4f2911c5cc7
Menglong TAN <tanmenglong@gmail.com>
2017-05-18 21:28:06 +0800

    up

--- thrift/nusadua.thrift
+++ thrift/nusadua.thrift
@@ -1,18 +1,18 @@
 namespace go nusadua.rpc
 
 exception RpcException {
-    1: i32 status,
-    2: string message
+  1: i32 status,
+  2: string message
 }
 
 
 service ParameterServer {
-    void create_task(1:string name, 2:i64 max_key) throws (1:RpcException ex),
+  void create_task(1:string name, 2:i64 max_key) throws (1:RpcException ex),
 
-    // work with parameters
-    void multi_push(1:list<list<i64>> keys, 2:list<double> values) throws (1:RpcException ex),
-    list<double> multi_pull(1:list<list<i64>> keys) throws (1:RpcException ex),
+  // work with parameters
+  void multi_push(1:list<list<i64>> keys, 2:list<double> values) throws (1:RpcException ex),
+  list<double> multi_pull(1:list<list<i64>> keys) throws (1:RpcException ex),
 
-    void range_push(1:i64 start_key, 2:i64 end_key, 3:list<double> values) throws (1:RpcException ex),
-    list<double> range_pull(1:i64 start_key, 2:i64 end_key) throws (1:RpcException ex)
+  void range_push(1:i64 start_key, 2:i64 end_key, 3:list<double> values) throws (1:RpcException ex),
+  list<double> range_pull(1:i64 start_key, 2:i64 end_key) throws (1:RpcException ex)
 }
